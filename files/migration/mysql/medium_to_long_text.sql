d21c66d11c0078e1347ee8dd5504e4b2a1e25743
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2015-08-17 14:14:09 +0200

    [migration] Migration from enum to ref tables
    - added tables that contain enum values
    - written migration script to handle transition
    
    Change-Id: I0d2b6acc708c368250ef1781f737d9067b783d99

--- files/migration/mysql/medium_to_long_text.sql
+++ files/migration/mysql/medium_to_long_text.sql
@@ -1,10 +1,3 @@
-alter table mon.alarm modify column state varchar(20) NOT NULL;
-alter table mon.alarm_action modify column alarm_state varchar(20) NOT NULL;
-alter table mon.notification_method modify column type varchar(20) NOT NULL;
-alter table mon.stream_actions modify column action_type varchar(20) NOT NULL;
-alter table mon.alarm_definition modify column severity varchar(20) NOT NULL;
-
--- applying conversion from medium text to longtext
 alter table mon.alarm_definition modify column expression longtext COLLATE utf8mb4_unicode_ci NOT NULL;
 alter table mon.sub_alarm modify column expression longtext COLLATE utf8mb4_unicode_ci NOT NULL;
 
